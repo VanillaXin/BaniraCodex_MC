5514f69e669d4e535f8dd147fdcc0e7c6a39c67e
辉小月 <TsukiMaaii@gmail.com>
2026-03-29 12:25:13 +0800

    refactor(enum): 更新枚举描述方法

--- src/main/java/xin/vanilla/banira/client/enums/EnumLogicalOperator.java
+++ src/main/java/xin/vanilla/banira/client/enums/EnumLogicalOperator.java
@@ -1,6 +1,7 @@
 package xin.vanilla.banira.client.enums;
 
 import lombok.experimental.Accessors;
+import xin.vanilla.banira.BaniraComponent;
 import xin.vanilla.banira.common.data.Component;
 import xin.vanilla.banira.common.enums.IEnumDescribable;
 import xin.vanilla.banira.common.util.EnumDescriptionHelper;
@@ -27,6 +28,6 @@ public enum EnumLogicalOperator implements IEnumDescribable {
 
     @Override
     public Component enumDescription() {
-        return EnumDescriptionHelper.describeEnum(this);
+        return EnumDescriptionHelper.describeEnum(BaniraComponent.get(), this);
     }
 }

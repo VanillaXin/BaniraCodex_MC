307984e6c2ad30768ea6066cfd51fce752fdc189
辉小月 <TsukiMaaii@gmail.com>
2026-03-29 12:25:13 +0800

    refactor(enum): 更新枚举描述方法

--- src/main/java/xin/vanilla/banira/client/enums/EnumCoordinateType.java
+++ src/main/java/xin/vanilla/banira/client/enums/EnumCoordinateType.java
@@ -1,5 +1,6 @@
 package xin.vanilla.banira.client.enums;
 
+import xin.vanilla.banira.BaniraComponent;
 import xin.vanilla.banira.common.data.Component;
 import xin.vanilla.banira.common.enums.IEnumDescribable;
 import xin.vanilla.banira.common.util.EnumDescriptionHelper;
@@ -32,6 +33,6 @@ public enum EnumCoordinateType implements IEnumDescribable {
 
     @Override
     public Component enumDescription() {
-        return EnumDescriptionHelper.describeEnum(this);
+        return EnumDescriptionHelper.describeEnum(BaniraComponent.get(), this);
     }
 }

7e0e72d297630977429e6ec0912b374a4f8be988
辉小月 <TsukiMaaii@gmail.com>
2026-03-30 16:47:03 +0800

    refactor(client): 重构客户端HUD渲染逻辑

--- src/main/java/xin/vanilla/banira/client/event/BaniraClientEventHub.java
+++ src/main/java/xin/vanilla/banira/client/event/BaniraClientEventHub.java
@@ -86,11 +86,6 @@ public final class BaniraClientEventHub {
             }
         });
         Client.onDrawScreenPost(event -> NotificationManager.get().render(event.getGuiGraphics().pose()));
-        Client.onRenderOverlayPost(event -> {
-            if (BaniraGuiOverlayEvent.PLAYER_LIST.equals(event.overlayId()) && Minecraft.getInstance().screen == null) {
-                NotificationManager.get().render(event.guiGraphics().pose());
-            }
-        });
     }
 
     public static void dispatchModClientSetup(FMLClientSetupEvent event) {

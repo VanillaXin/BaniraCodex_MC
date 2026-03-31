aaf184be7018b127d09371fbb01ec7a5339d4055
辉小月 <TsukiMaaii@gmail.com>
2026-03-31 20:00:41 +0800

    fix(lang): 修复客户端语言资源加载问题

--- src/main/java/xin/vanilla/banira/common/util/Translator.java
+++ src/main/java/xin/vanilla/banira/common/util/Translator.java
@@ -3,7 +3,6 @@ package xin.vanilla.banira.common.util;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import lombok.NonNull;
-import net.minecraft.client.Minecraft;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.server.packs.PackResources;
@@ -240,7 +239,12 @@ public class Translator implements ITranslator {
 
     private void loadFromResourceManager() {
         try {
-            ResourceManager manager = BaniraCodex.serverInstance().key().getResourceManager();
+            ResourceManager manager;
+            if (BaniraCodex.serverInstance().val()) {
+                manager = BaniraCodex.serverInstance().key().getResourceManager();
+            } else {
+                manager = net.minecraft.client.Minecraft.getInstance().getResourceManager();
+            }
             Collection<ResourceLocation> resources = collectModLangJsonLocations(manager);
             languages.addAll(resources.stream()
                     .filter(loc -> modId.equals(loc.getNamespace()))
@@ -333,7 +337,7 @@ public class Translator implements ITranslator {
      */
     public static String getClientLanguage() {
         if (FMLEnvironment.dist.isClient()) {
-            return normalizeLanguageCode(Minecraft.getInstance().getLanguageManager().getSelected().getCode());
+            return normalizeLanguageCode(net.minecraft.client.Minecraft.getInstance().getLanguageManager().getSelected().getCode());
         }
         return normalizeLanguageCode(CustomConfig.getDefaultLanguage());
     }

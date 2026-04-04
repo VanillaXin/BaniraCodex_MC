d6fcd6ecda8fd20a4d225e7db4865739ebc1fda2
辉小月 <TsukiMaaii@gmail.com>
2026-04-04 14:13:52 +0800

    fix(build): 修正Minecraft版本范围和自定义标签参数命名

--- src/main/java/xin/vanilla/banira/common/util/ItemUtils.java
+++ src/main/java/xin/vanilla/banira/common/util/ItemUtils.java
@@ -230,13 +230,13 @@ public final class ItemUtils {
      *
      * @param modId 模组ID
      */
-    public static void setCustomTag(String modId, @NonNull ItemStack item, CompoundTag CustomTag) {
+    public static void setCustomTag(String modId, @NonNull ItemStack item, CompoundTag customTag) {
         CompoundTag tag = item.getTag();
         if (tag == null) {
             tag = new CompoundTag();
             item.setTag(tag);
         }
-        tag.put(modId, CustomTag);
+        tag.put(modId, customTag);
     }
 
     /**

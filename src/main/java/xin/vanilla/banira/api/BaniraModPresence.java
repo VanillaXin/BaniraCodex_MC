1a90307198a51ba5c2fb98a6f4f993c60fe2c84e
辉小月 <TsukiMaaii@gmail.com>
2026-06-24 01:41:39 +0800

    refactor(api): expose stable mod presence facade

---
+++ src/main/java/xin/vanilla/banira/api/BaniraModPresence.java
@@ -0,0 +1,40 @@
+package xin.vanilla.banira.api;
+
+import xin.vanilla.banira.common.network.ModLoadedPresence;
+
+import javax.annotation.Nonnull;
+import java.util.List;
+import java.util.function.Consumer;
+
+/**
+ * 子 mod 用于声明“客户端也安装了本 mod”的稳定入口。
+ */
+public final class BaniraModPresence {
+
+    private BaniraModPresence() {
+    }
+
+    @Nonnull
+    public static BaniraModPresenceRegistration register(@Nonnull String modId, @Nonnull Consumer<Object> onServerSync) {
+        return ModLoadedPresence.registerInternal(modId, onServerSync)::close;
+    }
+
+    @Nonnull
+    public static BaniraModPresenceRegistration register(@Nonnull String modId) {
+        return register(modId, player -> {
+        });
+    }
+
+    public static boolean unregister(@Nonnull String modId) {
+        return ModLoadedPresence.unregisterInternal(modId);
+    }
+
+    public static boolean hasRegistration(@Nonnull String modId) {
+        return ModLoadedPresence.hasRegistrationInternal(modId);
+    }
+
+    @Nonnull
+    public static List<String> announcedModIds() {
+        return ModLoadedPresence.announcedModIdsInternal();
+    }
+}

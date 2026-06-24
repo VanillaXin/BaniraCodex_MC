ce144eee3e0b57294c869675f49ee0bd15f2fe42
辉小月 <TsukiMaaii@gmail.com>
2026-06-25 01:10:15 +0800

    fix(texture): harden forge 19.2 texture size lookup

--- src/main/java/xin/vanilla/banira/client/util/PNGUtils.java
+++ src/main/java/xin/vanilla/banira/client/util/PNGUtils.java
@@ -14,6 +14,7 @@ import java.util.zip.CRC32;
 import java.util.zip.Deflater;
 import java.util.zip.DeflaterOutputStream;
 import java.util.zip.InflaterInputStream;
+
 public final class PNGUtils {
     private PNGUtils() {
     }

528db5cb7c3cc4011598f9df898fd4ba1cb66b7c
辉小月 <TsukiMaaii@gmail.com>
2026-07-12 17:55:52 +0800

    Expose stable server runtime utilities

---
+++ src/main/java/xin/vanilla/banira/common/util/BaniraServerUtils.java
@@ -0,0 +1,46 @@
+package xin.vanilla.banira.common.util;
+
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.server.MinecraftServer;
+import xin.vanilla.banira.BaniraCodex;
+import xin.vanilla.banira.common.player.PlayerDataManager;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * 子 mod 可用的服务端运行时入口；具体状态存储方式由当前版本内部实现决定。
+ */
+public final class BaniraServerUtils {
+    private BaniraServerUtils() {
+    }
+
+    public static boolean isRunning() {
+        return BaniraCodex.serverInstance().val();
+    }
+
+    @Nullable
+    public static MinecraftServer currentServer() {
+        return BaniraCodex.serverInstance().key();
+    }
+
+    @Nonnull
+    public static List<ServerPlayerEntity> players() {
+        MinecraftServer server = currentServer();
+        return server == null ? Collections.emptyList() : server.getPlayerList().getPlayers();
+    }
+
+    @Nullable
+    public static ServerPlayerEntity player(@Nonnull UUID uuid) {
+        MinecraftServer server = currentServer();
+        return server == null ? null : server.getPlayerList().getPlayer(uuid);
+    }
+
+    @Nonnull
+    public static PlayerDataManager playerDataManager() {
+        return BaniraCodex.playerDataManager;
+    }
+}

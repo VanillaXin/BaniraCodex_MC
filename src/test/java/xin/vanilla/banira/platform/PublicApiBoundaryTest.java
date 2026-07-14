fc773d61b9bde752ace8036984181bb32a236f10
辉小月 <TsukiMaaii@gmail.com>
2026-07-14 09:39:07 +0800

    Adapt stable runtime facades to Fabric 1.16

--- src/test/java/xin/vanilla/banira/platform/PublicApiBoundaryTest.java
+++ src/test/java/xin/vanilla/banira/platform/PublicApiBoundaryTest.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 public class PublicApiBoundaryTest {

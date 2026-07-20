0c58ebaa8c61982330877f40c87595e78d13194b
辉小月 <TsukiMaaii@gmail.com>
2026-07-20 17:50:37 +0800

    Keep notifications readable and above quick actions

--- src/main/java/xin/vanilla/banira/client/gui/component/Notification.java
+++ src/main/java/xin/vanilla/banira/client/gui/component/Notification.java
@@ -121,6 +121,7 @@ public class Notification extends NotificationData {
         Notification n = new Notification(c);
         n.bgColor(Color.argb(ClientThemeManager.getEffectiveTheme().popupBg()));
         n.borderColor(Color.argb(ClientThemeManager.getEffectiveTheme().popupBorder()));
+        n.ensureReadableComponentColors();
         return n;
     }
 
@@ -153,6 +154,7 @@ public class Notification extends NotificationData {
         } else {
             n.bgColor(data.bgColor());
             n.borderColor(data.borderColor());
+            n.ensureReadableComponentColors();
         }
         n.themedFromNetwork(fromNetwork);
         return n;
@@ -212,7 +214,25 @@ public class Notification extends NotificationData {
         Component c = this.component().clone();
         c.color(Color.argb(textArgb));
         this.component(c);
+        this.ensureReadableComponentColors();
+    }
+
+    private void ensureReadableComponentColors() {
+        ensureReadableComponentColors(this.component(), this.bgColor().argb());
         this.updateRichLayout();
+    }
+
+    private static void ensureReadableComponentColors(@Nullable Component component, int backgroundArgb) {
+        if (component == null) {
+            return;
+        }
+        component.color(Color.argb(ColorUtils.ensureReadableTextArgb(component.color().argb(), backgroundArgb)));
+        for (Component child : component.getChildren()) {
+            ensureReadableComponentColors(child, backgroundArgb);
+        }
+        for (Component arg : component.getArgs()) {
+            ensureReadableComponentColors(arg, backgroundArgb);
+        }
     }
 
     private void updateRichLayout() {

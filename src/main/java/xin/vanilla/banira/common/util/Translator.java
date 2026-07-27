13980ac44eaf16d96928d4bad54669d180535954
辉小月 <TsukiMaaii@gmail.com>
2026-07-27 17:51:35 +0800

    refactor(config): 提取通用设置为独立API类

--- src/main/java/xin/vanilla/banira/common/util/Translator.java
+++ src/main/java/xin/vanilla/banira/common/util/Translator.java
@@ -19,6 +19,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.objectweb.asm.Type;
 import xin.vanilla.banira.BaniraCodex;
+import xin.vanilla.banira.api.BaniraCommonSettings;
 import xin.vanilla.banira.common.data.Component;
 import xin.vanilla.banira.common.data.ScopedComponent;
 import xin.vanilla.banira.common.enums.EnumI18nType;
@@ -378,14 +379,14 @@ public class Translator implements ITranslator {
         if (FMLEnvironment.dist.isClient()) {
             return normalizeLanguageCode(net.minecraft.client.Minecraft.getInstance().getLanguageManager().getSelected());
         }
-        return normalizeLanguageCode(CustomConfig.getDefaultLanguage());
+        return normalizeLanguageCode(BaniraCommonSettings.defaultLanguage());
     }
 
     /**
      * 获取服务端默认语言
      */
     public static String getServerLanguage() {
-        return normalizeLanguageCode(CustomConfig.getDefaultLanguage());
+        return normalizeLanguageCode(BaniraCommonSettings.defaultLanguage());
     }
 
     /**
@@ -405,7 +406,7 @@ public class Translator implements ITranslator {
                     : getClientLanguage();
         }
         if ("server".equalsIgnoreCase(language)) {
-            return normalizeLanguageCode(CustomConfig.getDefaultLanguage());
+            return normalizeLanguageCode(BaniraCommonSettings.defaultLanguage());
         }
         return normalizeLanguageCode(language);
     }
@@ -420,7 +421,7 @@ public class Translator implements ITranslator {
                     : CustomConfig.getPlayerLanguage(PlayerUtils.getPlayerUUIDString(player));
             return getValidLanguage(player, lang);
         } catch (IllegalArgumentException e) {
-            return normalizeLanguageCode(CustomConfig.getDefaultLanguage());
+            return normalizeLanguageCode(BaniraCommonSettings.defaultLanguage());
         }
     }
 

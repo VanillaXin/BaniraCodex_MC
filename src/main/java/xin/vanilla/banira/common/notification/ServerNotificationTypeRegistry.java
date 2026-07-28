36962dec978658092bdafeb6494a859af3a773ef
辉小月 <TsukiMaaii@gmail.com>
2026-07-28 12:36:39 +0800

    feat(notification): route built-in player messages through types

--- src/main/java/xin/vanilla/banira/common/notification/ServerNotificationTypeRegistry.java
+++ src/main/java/xin/vanilla/banira/common/notification/ServerNotificationTypeRegistry.java
@@ -29,6 +29,10 @@ public final class ServerNotificationTypeRegistry {
 
     static {
         KNOWN.add(NotificationTypeKeys.DEFAULT);
+        registerInternal(NotificationTypeKeys.HELP, EnumPosition.TOP_CENTER, EnumMoveType.AUTO,
+                EnumNotificationTypeDisplayMode.VANILLA_CHAT);
+        registerInternal(NotificationTypeKeys.COMMAND_FEEDBACK, EnumPosition.TOP_CENTER, EnumMoveType.AUTO,
+                EnumNotificationTypeDisplayMode.VANILLA_CHAT);
     }
 
     private ServerNotificationTypeRegistry() {

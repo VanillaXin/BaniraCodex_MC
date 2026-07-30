e421d43cd626df72c2888171cce90d5fcf70dea6
辉小月 <TsukiMaaii@gmail.com>
2026-07-30 16:12:57 +0800

    fix(notification): honor child mod themes

--- src/main/java/xin/vanilla/banira/client/gui/component/Notification.java
+++ src/main/java/xin/vanilla/banira/client/gui/component/Notification.java
@@ -9,6 +9,7 @@ import net.minecraft.util.IReorderingProcessor;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.Style;
 import xin.vanilla.banira.BaniraComponent;
+import xin.vanilla.banira.api.client.theme.BaniraThemes;
 import xin.vanilla.banira.client.data.BaniraColorConfig;
 import xin.vanilla.banira.client.data.ScreenCoordinate;
 import xin.vanilla.banira.client.data.ShapeDrawArgs;
@@ -74,7 +75,7 @@ public class Notification extends NotificationData {
      */
     private long coalesceLastActivityMs;
     /**
-     * 是否由网络包经 {@link #fromData} 应用了客户端主题色
+     * 是否经 {@link #fromData} 应用了客户端主题色
      */
     private boolean themedFromNetwork;
 
@@ -170,14 +171,16 @@ public class Notification extends NotificationData {
         n.acceleration(data.acceleration());
         n.decelerationDistance(data.decelerationDistance());
         n.notificationType(data.notificationType() != null ? data.notificationType() : NotificationTypeKeys.DEFAULT);
-        if (fromNetwork) {
+        boolean themed = fromNetwork || data.themed();
+        if (themed) {
             n.applyClientNotificationStyle(n.style());
         } else {
             n.bgColor(data.bgColor());
             n.borderColor(data.borderColor());
             n.ensureReadableComponentColors();
         }
-        n.themedFromNetwork(fromNetwork);
+        n.themed(themed);
+        n.themedFromNetwork(themed);
         return n;
     }
 
@@ -203,7 +206,7 @@ public class Notification extends NotificationData {
     }
 
     private void applyClientNotificationStyle(EnumNotificationStyle style) {
-        BaniraColorConfig t = ClientThemeManager.getEffectiveTheme();
+        BaniraColorConfig t = notificationTheme();
         int bg;
         int border;
         int textArgb;
@@ -239,6 +242,19 @@ public class Notification extends NotificationData {
         }
         this.component(c);
         this.ensureReadableComponentColors();
+    }
+
+    private BaniraColorConfig notificationTheme() {
+        String type = notificationType();
+        int separator = type != null ? type.indexOf(':') : -1;
+        if (separator < 0 && type != null) {
+            separator = type.indexOf('.');
+        }
+        if (separator > 0) {
+            return BaniraColorConfig.forSeason(
+                    BaniraThemes.seasonFor(type.substring(0, separator)));
+        }
+        return ClientThemeManager.getEffectiveTheme();
     }
 
     private void ensureReadableComponentColors() {
